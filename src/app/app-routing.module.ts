66c7fd06225fcff841b54ad6207b23efa0b21917
Matthew Waskin <matthew@waskin.com>
2023-10-20 10:24:52 -0400

    replace src dir with starter code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard-routing.module').then(
+        (mod) => mod.DashboardRoutingModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { WelcomeComponent } from './welcome/welcome.component';
+
 @Component({
+  standalone: true,
+  imports: [WelcomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'standalone-components-routing';
-}
+export class AppComponent {}
